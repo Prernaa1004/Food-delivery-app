f362c26a5614663220d3ec26b3d1de14365ab4cf
Arceus7080 <135990583+Arceus7080@users.noreply.github.com>
2024-07-09 23:13:47 +0530

    Admin PI

---
+++ ADMIN/app/src/main/java/com/example/adminzomato_clone/CreateUserActivity.kt
@@ -0,0 +1,27 @@
+package com.example.adminzomato_clone
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.adminzomato_clone.databinding.ActivityCreateUserBinding
+
+class CreateUserActivity : AppCompatActivity() {
+    private val binding: ActivityCreateUserBinding by lazy {
+        ActivityCreateUserBinding.inflate(layoutInflater)
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        binding.backButton.setOnClickListener{
+            finish()
+        }
+    }
+}
